a2cf9cd22c20fcfcf70cf5476b83b9ae89a9dd6c
Daniel Rogahn <draquix@yahoo.com>
2021-03-22 15:35:03 -0500

    scaffold

--- Leap Year Calculator/Program.cs
+++ Leap Year Calculator/Program.cs
@@ -10,6 +10,7 @@ namespace Leap_Year_Calculator
             {
                 Console.Write("Input a year to check if it is a leapyear: ");
                 int year = Convert.ToInt32(Console.ReadLine());
+                DateTime dt = new DateTime(year);
 
                 bool isLeapYear = false;
                 if ((year % 4 == 0 && !(year % 100 == 0)) || (year % 4 == 0 && year % 400 == 0))
@@ -17,7 +18,7 @@ namespace Leap_Year_Calculator
                     isLeapYear = true;
                 }
                 string answer = (isLeapYear) ? " is a leapyear." : " is not a leapyear.";
-                Console.WriteLine($"The year {year} {answer}");
+                Console.WriteLine($"The year {dt} {answer}");
             }
             Console.ReadLine();
         }
